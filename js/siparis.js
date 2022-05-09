a681e7ceaa96102a3ed348eac7fabe8bcacafca9
Orçun Madran <orcunmadran@gmail.com>
2022-05-09 16:52:12 +0300

    Menu created

---
+++ js/siparis.js
@@ -0,0 +1,78 @@
+$(function() {
+       $.getJSON('menu.json', function(data) {
+           $.each(data.sicakKahve, function(i, f) {
+           document.getElementById('sicakKahve').innerHTML +=
+           '<button type="button" class="btn btn-secondary">'+f.urunAdi+'</button>&nbsp&nbsp';
+         });
+           $.each(data.sogukKahve, function(i, f) {
+              var tblRow =
+              "<tr>" +
+              "<td>" + f.urunNo + "</td>" +
+              "<td>" + f.urunAdi + "</td>" +
+              "<td>" + f.urunFiyati + "</td>" +
+              "<td><button type='button' class='btn btn-success btn-sm'>" + "&#43" + "</button></td>" +
+              "</tr>"
+               $(tblRow).appendTo("#sogukKahve tbody");
+         });
+         $.each(data.cay, function(i, f) {
+              var tblRow =
+              "<tr>" +
+              "<td>" + f.urunNo + "</td>" +
+              "<td>" + f.urunAdi + "</td>" +
+              "<td>" + f.urunFiyati + "</td>" +
+              "<td><button type='button' class='btn btn-success btn-sm'>" + "&#43" + "</button></td>" +
+              "</tr>"
+               $(tblRow).appendTo("#cay tbody");
+         });
+         $.each(data.sogukIcecek, function(i, f) {
+              var tblRow =
+              "<tr>" +
+              "<td>" + f.urunNo + "</td>" +
+              "<td>" + f.urunAdi + "</td>" +
+              "<td>" + f.urunFiyati + "</td>" +
+              "<td><button type='button' class='btn btn-success btn-sm'>" + "&#43" + "</button></td>" +
+              "</tr>"
+               $(tblRow).appendTo("#sogukIcecek tbody");
+         });
+         $.each(data.mesrubat, function(i, f) {
+              var tblRow =
+              "<tr>" +
+              "<td>" + f.urunNo + "</td>" +
+              "<td>" + f.urunAdi + "</td>" +
+              "<td>" + f.urunFiyati + "</td>" +
+              "<td><button type='button' class='btn btn-success btn-sm'>" + "&#43" + "</button></td>" +
+              "</tr>"
+               $(tblRow).appendTo("#mesrubat tbody");
+         });
+         $.each(data.sandwich, function(i, f) {
+              var tblRow =
+              "<tr>" +
+              "<td>" + f.urunNo + "</td>" +
+              "<td>" + f.urunAdi + "</td>" +
+              "<td>" + f.urunFiyati + "</td>" +
+              "<td><button type='button' class='btn btn-success btn-sm'>" + "&#43" + "</button></td>" +
+              "</tr>"
+               $(tblRow).appendTo("#sandwich tbody");
+         });
+         $.each(data.tatli, function(i, f) {
+              var tblRow =
+              "<tr>" +
+              "<td>" + f.urunNo + "</td>" +
+              "<td>" + f.urunAdi + "</td>" +
+              "<td>" + f.urunFiyati + "</td>" +
+              "<td><button type='button' class='btn btn-success btn-sm'>" + "&#43" + "</button></td>" +
+              "</tr>"
+               $(tblRow).appendTo("#tatli tbody");
+         });
+         $.each(data.salata, function(i, f) {
+              var tblRow =
+              "<tr>" +
+              "<td>" + f.urunNo + "</td>" +
+              "<td>" + f.urunAdi + "</td>" +
+              "<td>" + f.urunFiyati + "</td>" +
+              "<td><button type='button' class='btn btn-success btn-sm'>" + "&#43" + "</button></td>" +
+              "</tr>"
+               $(tblRow).appendTo("#salata tbody");
+         });
+       });
+    });
